408ce3efc3a70bcdee35ca1352d8f0c41db97131
ruthwiik <88970276+ruthwiik@users.noreply.github.com>
2021-09-02 13:34:48 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+require('@firebase/firestore')
+const firebaseConfig = {
+    apiKey: "AIzaSyCPGMaejvPmtAETnqvCOp-XhLDOOnvCKlA",
+    authDomain: "book-santa-dbaba.firebaseapp.com",
+    projectId: "book-santa-dbaba",
+    storageBucket: "book-santa-dbaba.appspot.com",
+    messagingSenderId: "1042660676383",
+    appId: "1:1042660676383:web:d55211bd3f7b5d2d52562a"
+  };
+  firebase.initializeApp(firebaseConfig);
+   export default firebase.firestore();
